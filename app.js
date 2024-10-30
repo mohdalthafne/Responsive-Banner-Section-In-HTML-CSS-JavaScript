44de24ff64f73595bac288e8f5ac84d41f93adfa
mohdalthafne <mohdalthafne@gmail.com>
2024-10-30 06:32:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let navBtn = document.querySelector("#navBtn");
+let navBar = document.querySelector("#navBar");
+
+navBtn.onclick = () => {
+    navBar.classList.toggle("show")
+    navBtn.classList.toggle("fa-xmark")
+}
